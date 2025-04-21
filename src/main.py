645e451c0d068956bb8971128646ecb480e96d3e
pooret-animl <tyler.poore@animl.health>
2025-04-21 01:11:56 -0400

    updated main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
 from grader import PDFSummaryGrader
-import json
 
-
-
+google_api_key = "ASDRQVASdgfaeraVASFA"
 summaries_path = "summary_path"
-graded_file_name = summaries_path.strip(".json").split("/")[-1] # put in class
-grader = PDFSummaryGrader(google_api_key) # include .env
-with open(summaries_path, 'r') as f:
-    summaries_dict = json.load(f)
-results = grader.create_grading_report(summaries_dict)
+grader = PDFSummaryGrader(api_key = google_api_key)
+results = grader.create_grading_report(summaries_path)
 if __name__ == "__main__":
+    pass
