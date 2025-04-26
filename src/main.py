28efa53020837cf173a47d4cc5403ce22d1db883
pooret-animl <tyler.poore@animl.health>
2025-04-26 16:26:23 -0400

    updated grader

--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ google_api_key = "ASDRQVASdgfaeraVASFA"
 summaries_path = "summary_path"
 grader = PDFSummaryGrader(api_key = google_api_key)
 results = grader.create_grading_report(summaries_path)
+
 if __name__ == "__main__":
     pass
